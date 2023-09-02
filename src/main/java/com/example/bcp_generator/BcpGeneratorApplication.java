a887791d6055bd11f76c6878ee2f15e57ab9c0d7
devaliel <darkzill0410@gmail.com>
2023-09-03 00:08:56 +0800

    initial commit

---
+++ src/main/java/com/example/bcp_generator/BcpGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.bcp_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcpGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcpGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bcp_generator/BcpGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bcp_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcpGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
